161f8d7cdb6a77a146b72e8a991af166b4960e73
marouenbg <marouen.b.guebila@gmail.com>
2022-07-07 14:58:34 -0400

    rebuilding site Thu Jul  7 14:58:34 EDT 2022

--- fetchPubs.py
+++ fetchPubs.py
@@ -185,6 +185,35 @@ if 1: #scholarly error
 	        except:
         	        print("Paper has no url")
 
+## 7. YARN
+yarn = re.compile("Tissue-aware RNA-Seq processing and normalization for heterogeneous and sparse data")
+
+# Retrieve the author's data, fill-in, and print
+search_query = scholarly.search_author('Joseph N Paulson')
+fauthor = next(search_query)
+author = scholarly.fill(fauthor)
+
+pubs = pubs + '### YARN \n \n'
+
+# Print the titles of the author's publications
+for pub in author['publications']:
+        if yarn.search(pub['bib']['title']):
+                break;
+
+# Take a closer look at the first publication
+first_publication_filled = scholarly.fill(pub)
+scholarly.pprint(first_publication_filled)
+
+# Fetch citations
+for citation in scholarly.citedby(first_publication_filled):
+        print(citation['bib']['title'])
+        try:
+                pubs = pubs + '>'+citation['bib']['title']+'. '+citation['bib']['author']+'. \n \n'+citation['bib']['url'] + '. \n \n'
+        except:
+                print("Paper has no url")
+
+time.sleep(30)
+
 # FIN. save to markdown
 text_file = open("netZooWeb/content/papers/papers.md","w")
 text_file.write(pubs)
